e8aebb5e0ce4137d8bf971e04b0e23cea15d92d9
cnb <cneirabustos@gmail.com>
2015-09-12 12:19:11 -0300

    create restful service with h2 databse

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [ring/ring-json "0.4.0"]
                  [org.immutant/web "2.1.0"]
                  [com.h2database/h2 "1.3.170"]
-                 [org.clojure/java.jdbc "0.4.1"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [java-jdbc/dsl "0.1.0"]
                  ]
   :plugins [[lein-ring "0.8.13"] [lein-immutant "2.0.0"]]
   :ring {:handler restful-workshop.handler/app}
--- src/restful_workshop/handler.clj
+++ src/restful_workshop/handler.clj
@@ -1,28 +1,56 @@
+
 (ns restful-workshop.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.util.response :refer [resource-response response]]
             [ring.middleware.json :as middleware]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults api-defaults]]
-            [immutant.web :as web])
+            [immutant.web :as web]
+            [restful-workshop.dbaccess :as db]
+            )
   )
 
+(defn return-params [request]
+  (if (empty? (request :body))
+    (request :params)
+    (request :body)))
+
+
+; We are expecting this json,
+; { “ssn”: <a ssn for the individual> ,
+;   “first_name”: <first_name>,
+;   “last_name”: <last_name>,
+;   “timestamp”: <from when to check records>,
+;   “type_of_crime”: <economic/murder/other>
+;
+;  }
+
+
+(defn generate-response [request]
+  (let [{:keys [ssn first_name last_name timestamp type_of_crime]} (return-params request)]
+    {:x ssn :y first_name :z type_of_crime}))
+
+(defn return-criminal [request]
+  (db/get-criminal (return-params request))
+  )
+
+
 (defroutes app-routes
-           (GET "/" [] (response {:what "Hello World"}))
-           (POST "/consumer" request (let [ssn (or (get-in request [:params :ssn])
-                                                   (get-in request [:body :ssn])
-                                                   "00000")]
-                                       {:status 200
-                                        :body   {:name ssn
-                                                 :desc (str "ssn received " ssn)}}))
+
+           (POST "/profiles" request {:status 200
+                                      :body   (return-criminal request)
+                                      })
+
            (route/not-found "Not Found"))
+
 
 (def app
   (-> app-routes
+      ; {:keywords? true} when the body is parsed makes the keys return as keywords
+      ; and not as strings
       (middleware/wrap-json-body {:keywords? true})
       (middleware/wrap-json-response)
-      (wrap-defaults api-defaults)
-      ))
+      (wrap-defaults api-defaults)))
 
 
 (defn start []
